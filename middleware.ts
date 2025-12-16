5f5920030a585d28f66f9196e3156bc691d502ed
DarkoMilevski89 <milevskidarko89@gmail.com>
2025-12-16 14:44:32 +0100

    Fix middleware to redirect to login instead of throwing 500

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,9 @@ import type { NextRequest } from "next/server";
 import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
 
 // Middleware to handle authentication
-export default withMiddlewareAuthRequired();
+export default withMiddlewareAuthRequired({
+  returnTo: "/api/auth/login"
+});
 
 export const config = {
   matcher: ["/builder/:path*", "/preview/:path*"],
